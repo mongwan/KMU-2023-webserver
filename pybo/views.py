ac99a0127d3ca6f9820f6ca167c20ceb566e919e
mongwan <me@mongwan.dev>
2023-03-22 12:58:40 +0900

    2023-03-22

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 def index(req):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(req, 'pybo/question_list.html', context)
+
+def detail(req, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(req, 'pybo/question_detail.html', context)
