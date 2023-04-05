4a38b62be6f28d2f304a42de4a83987ca9dab969
mongwan <me@mongwan.dev>
2023-04-05 15:25:25 +0900

    2023-03-29 make upload forms

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question
 from django.utils import timezone
+from .models import Question
+from .forms import QuestionForm, AnswerForm
 
 def index(req):
     question_list = Question.objects.order_by('-create_date')
@@ -14,5 +15,30 @@ def detail(req, question_id):
 
 def answer_create(req, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=req.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if req.method == "POST":
+        form = AnswerForm(req.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    
+    context = {'question': question, 'form': form}
+    return render(req, 'pybo/question_detail.html', context)
+
+def question_create(req):
+    if req.method == "POST":
+        form = QuestionForm(req.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    
+    context = {'form': form}
+    return render(req, 'pybo/question_form.html', context)
